e78a696cc317f16a2225e999039da5a873d04e98
YASHKALUNKHE <154042312+YASHKALUNKHE@users.noreply.github.com>
2025-07-03 11:56:25 +0530

    feat: added input forms for audio and media upload

--- public/script.js
+++ public/script.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const toggle = document.getElementById("themeToggle");
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
-=======
-const toggle = document.getElementById("themeToggle");
 
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
->>>>>>> 1f8443db1e820bcf318262bcc6b389acc2c753c9
